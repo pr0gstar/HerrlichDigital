039dbb16ea2a30e4e76216048d176fdca36919c3
Christoph Planken <c.planken@absturzsicherung.de>
2025-03-10 12:59:18 +0100

    add mouce over for hovering buttons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -35,7 +35,7 @@ export default function Index() {
           <p className="leading-6 text-gray-700 dark:text-gray-200">
             What&apos;s next?
           </p>
-          <Button>click me!</Button>
+          <Button onClick={() => alert("test")}>click me!</Button>
           <ul>
             {resources.map(({ href, text, icon }) => (
               <li key={href}>
