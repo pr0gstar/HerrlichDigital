0d124615faf54d2234230e0acd5d0cae514b7ea4
Christoph Planken <c.planken@absturzsicherung.de>
2025-03-13 12:25:13 +0100

    changed style for header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 
 export default function Header() {
   return (
-    <div className="px-9 py-9 lg:py-12">
+    <div className="p-10">
       <nav className="text-primary mx-auto flex max-w-8xl items-center justify-between">
         <div className="flex justify-center gap-4 align-middle">
           <Link
@@ -12,7 +12,7 @@ export default function Header() {
             <h1>Herrlich Digital</h1>
           </Link>
         </div>
-        <ul className="hidden lg:flex">
+        <ul className="flex">
           <li className="px-5 py-2">
             <Link to="/blog" className="text-gray-800 dark:text-gray-100">
               Blog
