b1646b472232d63ddf2b2faeffd53f914475daaa
Christoph Planken <c.planken@absturzsicherung.de>
2025-03-13 12:25:56 +0100

    add pages for blog and about

---
+++ app/routes/about.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="relative mx-20">
+      <h1>About</h1>
+    </div>
+  );
+}
---
+++ app/routes/blog.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="relative mx-20">
+      <h1>Blog</h1>
+    </div>
+  );
+}
